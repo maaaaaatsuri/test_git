35c15b5d34d15940f36bfc403618975d3701a36f
maaaaaatsuri <aaa@exaple.com>
2026-03-05 09:15:48 +0900

    練習：TypeScriptでTodoアプリ作成

--- src/start/Example.tsx
+++ src/start/Example.tsx
@@ -1,35 +1,32 @@
-import Profile from "./components/Profile";
+import { useState } from "react";
+import TaskList from "./TaskList";
 
-const profiles = [
-  {
-    name: "Geo",
-    age: 18,
-    hobbies: ["sports", "music"],
-  },
-  {
-    name: "Tom",
-    age: 25,
-    hobbies: ["movie", "music"],
-  },
-  {
-    name: "Lisa",
-    age: 21,
-    hobbies: ["sports", "travel", "game"],
-  },
-];
+export type Todo = {
+  id: number,
+  text: string
+}
 
 const Example = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 0, text: "Todo1"},
+    { id: 1, text: "Todo2"},
+  ]);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => setInputText(e.target.value)
+
+  const addTodoItem = () => {
+    setTodos(state => [...state, { id: state.length, text: inputText }])
+    setInputText("");
+  }
+
   return (
     <>
-      <ul>
-        {profiles.map((profile) => (
-          <li key={profile.name}>
-          <Profile {...profile} />
-          </li>
-        ))}
-      </ul>
+      <input type="text" value={inputText} onChange={changeHandler} />
+      <button onClick={addTodoItem}>追加</button>
+      <TaskList todos={todos} />
     </>
-  );
+  )
 };
 
 export default Example;
